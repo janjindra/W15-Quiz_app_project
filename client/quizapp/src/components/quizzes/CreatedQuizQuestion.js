eb84b141b5889fb0942e2c851d1dc6942b3fa3e7
Jan Jindra <jindra.jan1993@gmail.com>
2020-04-11 20:23:19 +0100

    2 options for true false type of questions

--- client/quizapp/src/components/quizzes/CreatedQuizQuestion.js
+++ client/quizapp/src/components/quizzes/CreatedQuizQuestion.js
@@ -5,21 +5,45 @@ const CreatedQuizQuestion = (props) => {
 
 
   const questions = props.quiz.questions.map((question, index) => {
-    return (
-      <li key={index} className="component-item">
-        <div className="component">
-        <Fragment>
-        <h1>
-          {question.questionName}
-        </h1>
-        <p>Option A: {question.incorrectAnswers[0]}</p>
-        <p>Option B: {question.incorrectAnswers[1]}</p>
-        <p>Option C: {question.incorrectAnswers[2]}</p>
-        <p>Option D-correct: {question.correctAnswer}</p>
-        </Fragment>
-        </div>
-      </li>
-    )
+ if (question.type === "boolean") {
+
+//this is a true/false type of questions.
+   return (
+     <li key={index} className="component-item">
+       <div className="component">
+       <Fragment>
+       <h1>
+         {question.questionName}
+       </h1>
+       <p>Option A: {question.incorrectAnswers[0]}</p>
+       <p>Option B-correct: {question.correctAnswer}</p>
+       </Fragment>
+       </div>
+     </li>
+   )
+
+ }
+ else {
+//this is a multiple answer type of questions.
+   return (
+
+     <li key={index} className="component-item">
+       <div className="component">
+       <Fragment>
+       <h1>
+         {question.questionName}
+       </h1>
+       <p>Option A: {question.incorrectAnswers[0]}</p>
+       <p>Option B: {question.incorrectAnswers[1]}</p>
+       <p>Option C: {question.incorrectAnswers[2]}</p>
+       <p>Option D-correct: {question.correctAnswer}</p>
+       </Fragment>
+       </div>
+     </li>
+   )
+
+ }
+
   })
 
   return (
