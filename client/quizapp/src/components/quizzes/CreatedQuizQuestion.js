0dcfe99ab1bb2d3e03659ed53b56a9f3a1d4f4a9
Jan Jindra <jindra.jan1993@gmail.com>
2020-04-11 19:15:32 +0100

    all questions for a selected created quiz appear

--- client/quizapp/src/components/quizzes/CreatedQuizQuestion.js
+++ client/quizapp/src/components/quizzes/CreatedQuizQuestion.js
@@ -1,21 +1,32 @@
 import React, {Fragment}  from 'react';
 
-
 const CreatedQuizQuestion = (props) => {
-
   if(!props.quiz) { return "No created quiz selected." }
 
 
+  const questions = props.quiz.questions.map((question, index) => {
+    return (
+      <li key={index} className="component-item">
+        <div className="component">
+        <Fragment>
+        <h1>
+          {question.questionName}
+        </h1>
+        <p>Option A: {question.incorrectAnswers[0]}</p>
+        <p>Option B: {question.incorrectAnswers[1]}</p>
+        <p>Option C: {question.incorrectAnswers[2]}</p>
+        <p>Option D-correct: {question.correctAnswer}</p>
+        </Fragment>
+        </div>
+      </li>
+    )
+  })
+
   return (
-    <Fragment>
-    <h4>
-      {props.quiz.questions[0].questionName}
-    </h4>
-    <p>Option A: {props.quiz.questions[0].incorrectAnswers[0]}</p>
-    <p>Option B: {props.quiz.questions[0].incorrectAnswers[1]}</p>
-    <p>Option C: {props.quiz.questions[0].incorrectAnswers[2]}</p>
-    <p>Option D-correct: {props.quiz.questions[0].correctAnswer}</p>
-    </Fragment>
+    <ul className="component-list">
+      {questions}
+    </ul>
+
   )
 }
 
