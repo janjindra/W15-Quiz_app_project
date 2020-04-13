b96e0d3c250ca3e31f70a9c6a45331b97ef4394d
Jan Jindra <jindra.jan1993@gmail.com>
2020-04-13 21:25:33 +0100

    true and false type of questions have no blank options

--- client/quizapp/src/components/quizzes/CreatedQuizQuestion.js
+++ client/quizapp/src/components/quizzes/CreatedQuizQuestion.js
@@ -2,7 +2,6 @@ import React, {Component, Fragment} from 'react';
 import isEmpty from '../../helpers/is-empty';
 import ResultSummary from '../results/ResultSummary';
 
-
 class CreatedQuizQuestion extends Component{
   constructor(props){
     super(props);
@@ -25,10 +24,6 @@ class CreatedQuizQuestion extends Component{
     this.handleOptionClick = this.handleOptionClick.bind(this);
     this.handleNextButtonClick = this.handleNextButtonClick.bind(this);
     this.handlePreviousButtonClick = this.handlePreviousButtonClick.bind(this);
-    // this.endGame = this.endGame.bind(this);
-    // this.handleFinish = this.handleFinish.bind(this);
-
-
   };
 
 
@@ -68,43 +63,43 @@ class CreatedQuizQuestion extends Component{
     if(this.props.quiz){
       this.getDerivedStateFromProps(this.props, this.state);
 
+    }
   }
-}
 
 
-//the function sortList sorts/randomizes the 4 options for each question.
- sortList() {
-  var list, i, switching, b, shouldSwitch;
-  list = document.getElementById("id01");
-  switching = true;
-  /* Make a loop that will continue until
-  no switching has been done: */
-  while (switching) {
-    // start by saying: no switching is done:
-    switching = false;
-    b = list.getElementsByTagName("LI");
-    // Loop through all list-items:
-    for (i = 0; i < (b.length - 1); i++) {
-      // start by saying there should be no switching:
-      shouldSwitch = false;
-      /* check if the next item should
-      switch place with the current item: */
-      if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
-        /* if next item is alphabetically
-        lower than current item, mark as a switch
-        and break the loop: */
-        shouldSwitch = true;
-        break;
+  //the function sortList sorts/randomizes the 4 options for each question.
+  sortList() {
+    var list, i, switching, b, shouldSwitch;
+    list = document.getElementById("id01");
+    switching = true;
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+      // start by saying: no switching is done:
+      switching = false;
+      b = list.getElementsByTagName("LI");
+      // Loop through all list-items:
+      for (i = 0; i < (b.length - 1); i++) {
+        // start by saying there should be no switching:
+        shouldSwitch = false;
+        /* check if the next item should
+        switch place with the current item: */
+        if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+          /* if next item is alphabetically
+          lower than current item, mark as a switch
+          and break the loop: */
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark the switch as done: */
+        b[i].parentNode.insertBefore(b[i + 1], b[i]);
+        switching = true;
       }
     }
-    if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark the switch as done: */
-      b[i].parentNode.insertBefore(b[i + 1], b[i]);
-      switching = true;
-    }
   }
-}
 
 
 
@@ -174,91 +169,123 @@ class CreatedQuizQuestion extends Component{
   }
 
 
-// endGame() {
-//   alert('Quiz has ended!');
-//   const {state} = this;
-//   const playerStats = {
-//     score: state.score,
-//     numberOfQuestions: state.numberOfQuestions,
-//     numberOfAnsweredQuestions: state.numberOfAnsweredQuestions,
-//     correctAnswers: state.correctAnswers,
-//     wrongAnswers: state.wrongAnswers
-//   }
-//   console.log(playerStats);
-//   setTimeout(()=>{
-//      window.location = '/quizzes/results'
-//
-//   }, 1000);
-// }
+  // endGame() {
+  //   alert('Quiz has ended!');
+  //   const {state} = this;
+  //   const playerStats = {
+  //     score: state.score,
+  //     numberOfQuestions: state.numberOfQuestions,
+  //     numberOfAnsweredQuestions: state.numberOfAnsweredQuestions,
+  //     correctAnswers: state.correctAnswers,
+  //     wrongAnswers: state.wrongAnswers
+  //   }
+  //   console.log(playerStats);
+  //   setTimeout(()=>{
+  //      window.location = '/quizzes/results'
+  //
+  //   }, 1000);
+  // }
 
-// handleFinish(){
-// const playerStats = {
-//   score: this.state.score,
-//   numberOfQuestions: this.state.numberOfQuestions,
-//   numberOfAnsweredQuestions: this.state.numberOfAnsweredQuestions,
-//   correctAnswers: this.state.correctAnswers,
-//   wrongAnswers: this.state.wrongAnswers
-// }
-// console.log(playerStats);
-// window.open('/quizzes/results')
-// // history.push('/quizzes/results');
-// // window.location.href = '/quizzes/results'
-// // window.location.reload(false);
-// }
+  // handleFinish(){
+  // const playerStats = {
+  //   score: this.state.score,
+  //   numberOfQuestions: this.state.numberOfQuestions,
+  //   numberOfAnsweredQuestions: this.state.numberOfAnsweredQuestions,
+  //   correctAnswers: this.state.correctAnswers,
+  //   wrongAnswers: this.state.wrongAnswers
+  // }
+  // console.log(playerStats);
+  // window.open('/quizzes/results')
+  // // history.push('/quizzes/results');
+  // // window.location.href = '/quizzes/results'
+  // // window.location.reload(false);
+  // }
 
 
   render(){
     if(!this.props.quiz) { return "Please select a quiz above." }
 
     const {currentQuestion } = this.state;
+    
+    if (this.state.currentQuestionIndex<this.state.numberOfQuestions) {
+
+      if (this.state.currentQuestion.incorrectAnswers.length !=1){
+
+        // this is multiple choice answer
+        return (
+
+          <div className="options">
+          <h4>Question {this.state.currentQuestionIndex+1}/{this.state.numberOfQuestions}</h4>
+
+          <h4>{currentQuestion.questionName}</h4>
+          <ul id="id01">
+          <li><p onClick={this.handleOptionClick}>{currentQuestion.incorrectAnswers[0]}</p></li>
+          <li><p onClick={this.handleOptionClick}>{currentQuestion.incorrectAnswers[1]}</p></li>
+          <li><p onClick={this.handleOptionClick}>{currentQuestion.incorrectAnswers[2]}</p></li>
+          <li><p onClick={this.handleOptionClick}>{currentQuestion.correctAnswer}</p></li>
+          </ul>
+
+          <div>
+          <button type="button" onClick={this.handleQuitButtonClick}>Quit quiz</button>
+          <button type="button" onClick={this.handleNextButtonClick}>Skip this question</button>
 
 
-if (this.state.currentQuestionIndex<this.state.numberOfQuestions) {
+          </div>
+
+          </div>
+
+        )
+      }
+      else {
+
+        //this is true/false choice answer
+        return (
+
+          <div className="options">
+          <h4>Question {this.state.currentQuestionIndex+1}/{this.state.numberOfQuestions}</h4>
+
+          <h4>{currentQuestion.questionName}</h4>
+          <ul id="id01">
+          <li><p onClick={this.handleOptionClick}>{currentQuestion.incorrectAnswers[0]}</p></li>
+          <li><p onClick={this.handleOptionClick}>{currentQuestion.correctAnswer}</p></li>
+          </ul>
+
+          <div>
+          <button type="button" onClick={this.handleQuitButtonClick}>Quit quiz</button>
+          <button type="button" onClick={this.handleNextButtonClick}>Skip this question</button>
+
+
+          </div>
+
+          </div>
+
+        )
+      }
+
+    }
+
+    else {
+      window.alert("The quiz has ended");
+
+      return(
+
+        <Fragment>
+
+        <ResultSummary numberOfAnsweredQuestions={this.state.numberOfAnsweredQuestions}
+        correctAnswers={this.state.correctAnswers}
+        wrongAnswers={this.state.wrongAnswers}
+        numberOfQuestions={this.state.numberOfQuestions}
+        currentQuestionIndex={this.state.currentQuestionIndex}
+        currentQuestion={this.state.currentQuestion}
+        latestUser={this.state.latestUser}/>
+        </Fragment>
+
+      )
+    }
 
 
 
-    return (
 
-      <div className="options">
-      <h4>Question {this.state.currentQuestionIndex+1}/{this.state.numberOfQuestions}</h4>
-
-      <h4>{currentQuestion.questionName}</h4>
-      <ul id="id01">
-        <li><p onClick={this.handleOptionClick}>{currentQuestion.incorrectAnswers[0]}</p></li>
-        <li><p onClick={this.handleOptionClick}>{currentQuestion.incorrectAnswers[1]}</p></li>
-        <li><p onClick={this.handleOptionClick}>{currentQuestion.incorrectAnswers[2]}</p></li>
-        <li><p onClick={this.handleOptionClick}>{currentQuestion.correctAnswer}</p></li>
-      </ul>
-
-      <div>
-      <button type="button" onClick={this.handleQuitButtonClick}>Quit quiz</button>
-      <button type="button" onClick={this.handleNextButtonClick}>Skip this question</button>
-
-
-      </div>
-
-      </div>
-
-    )
-  }
-
-  else {
-  window.alert("The quiz has ended");
-
-    return(
-
-    <Fragment>
-    <ResultSummary numberOfAnsweredQuestions={this.state.numberOfAnsweredQuestions}
-    correctAnswers={this.state.correctAnswers}
-    wrongAnswers={this.state.wrongAnswers}
-    numberOfQuestions={this.state.numberOfQuestions}
-    currentQuestionIndex={this.state.currentQuestionIndex}
-    currentQuestion={this.state.currentQuestion}
-    latestUser={this.state.latestUser}/>
-    </Fragment>
-
-    )
-  }
   }
 
 }
