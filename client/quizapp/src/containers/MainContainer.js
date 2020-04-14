ffe18d72122481e1cc4016b013512ab581bee354
Peter Gemmell <petergemmell65@hotmail.com>
2020-04-14 15:10:17 +0100

    sidebar added

--- client/quizapp/src/containers/MainContainer.js
+++ client/quizapp/src/containers/MainContainer.js
@@ -7,24 +7,29 @@ import CreateQuestionContainer from './CreateQuestionContainer';
 import ProfileContainer from './ProfileContainer';
 import ResultSummary from '../components/results/ResultSummary';
 
-const MainContainer = () => {
-
-    return (
-      <Router>
-      <Fragment>
-      <NavBar/>
-      <Switch>
-
-      <Route path="/profile" component={ProfileContainer} />
-      <Route path="/quizzes" component={QuizzesContainer}/>
-      <Route path="/createQuestions" component={CreateQuestionContainer}/>
-      <Route path="/createQuizzes" component={CreateQuizContainer}/> // comment back in.
-      </Switch>
 
 
-      </Fragment>
-      </Router>
-    )
-}
+const MainContainer=() =>{
+
+  return (
+       <Router>
+       <Fragment>
+       <NavBar/>
+       <Switch>
+
+       <Route path="/profile" component={ProfileContainer} />
+       <Route path="/quizzes" component={QuizzesContainer}/>
+       <Route path="/createQuestions" component={CreateQuestionContainer}/>
+       <Route path="/createQuizzes" component={CreateQuizContainer}/> // comment back in.
+       </Switch>
+
+
+       </Fragment>
+       </Router>
+     )
+
+ }
+
+
 
 export default MainContainer;
