98145262667e22af4dec6cf6efe023b78767bbc2
user <user@users-MacBook-Pro.local>
2020-04-15 15:08:27 +0100

    CSS changes and links added in sidebar

--- client/quizapp/src/containers/LeftSidebar.js
+++ client/quizapp/src/containers/LeftSidebar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Sidebar from "react-sidebar";
 
 const navLinks = [
+  { url: 'https://imgflip.com/i/3wpwdi', name: 'Select Resources below:' },
   { url: 'https://www.nationalgeographic.com/', name: 'Geography' },
   { url: 'https://www.history.co.uk/', name: 'History' },
   { url: 'https://www.britannica.com/', name: 'General-Knowledge' },
@@ -12,7 +13,11 @@ const navLinks = [
   { url: 'https://a-z-animals.com/', name: 'Animals' },
   { url: 'https://www.whatcar.com/', name: 'Vehicles' },
   { url: 'https://www.brainpop.com/artsandmusic/', name: 'Entertainment: Music' },
+  { url: 'https://www.imdb.com/', name: 'Entertainment: Film' },
   { url: 'https://www.sciencedaily.com/', name: 'Science: Mathematics' },
+  { url: 'https://www.nationalgeographic.com/', name: 'Science: Nature' },
+  { url: 'https://www.chemistryworld.com/', name: 'Science: Chemistry' },
+  { url: 'https://www.sciencemag.org/category/biology', name: 'Science: Biology' },
   { url: 'https://www.eonline.com/uk/news', name: 'Celebrities' }
 ];
 
@@ -33,7 +38,7 @@ class LeftSidebar extends Component {
     return (
       <div>
         <div className={this.state.style}>
-        
+
           <ul className="sidebar-list-ul">
             {navLinks.map(({ url, name }) => (
               <li className="sidebar-list-li">
